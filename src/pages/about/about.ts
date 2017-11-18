8e7b5d28a3477c9ac075c9f04570208946112ece
carla yahuara <carlita_2033@hotmail.com>
2017-11-18 12:06:07 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+  nombre: string = "CARLA";
+  ocupacion: string = "Estudiante";
+  edad: number = 16;
+
+  datos: any = {
+    nombre: "Carla Monge",
+    ocupacion: "Estudiante",
+    edad: 16
+  };
+  Origen: string = "Lima-Perú";
+  Email: string = "carlita_2033@hotmail.com";
+  Telefono: number = 924769638;
+
   constructor(public navCtrl: NavController) {
 
   }
